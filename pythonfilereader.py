613ca5a57831ee36cf84754a34c32785a61388ab
DMorg5598 <113949079+DMorg5598@users.noreply.github.com>
2022-10-12 18:24:31 -0500

    Update pythonfilereader.py

--- pythonfilereader.py
+++ pythonfilereader.py
@@ -1,17 +1,35 @@
 ## ATTEMPT 1
-visit1 = []
-visit2 = []
+visits = []
+visit1 = 0
+visit2 = 0
 strikes = 0
 totalvisits = 0
+name1 = 0
+name2 = 0
+time1 = 0
+time2 = 0
+x = 1
+## ReadCycle Function
 f = open("usage.txt", "r")
-visit1 = f.readline()
-visit2 = f.readline()
-a = visit1[4]
-b = visit2[4]
-if a == b: 
-    print("These names match")
-else:
-    print("these names do not match")
+while x in f:
+    visits.append(f.readline())
+    totalvisits += 1
+    continue
+## NameMatcher Function
+def namechecker(name1,name2)
+    if name1 == name2:
+        continue
+myfunction()
+## TimeMatcher Function
+        time1 = visit1[18:35]
+        time2 = visit2[18:35]
+        if time1 == time2:
+            stikes += 1
+    else:
+        continue
+print(strikes)
+print("Total visit = ")
+print(totalvisits)
 
 ## ATTEMPT 2
 strikes = 0
@@ -26,7 +44,15 @@ else:
  print (totalvisits)
 
 ## TEXT FILE
-1, "Cinna Mon", 10/11/2022 15:53:00 PM
-2, "hazel nut", 10/11/2022 16:01:00 PM
-3, "hazel nut", 10/11/2022 16:02:30 PM
-4, "Bay Guette", 10/11/2022 16:04:50 PM
+1, "Cinna Mon", 10/11/2022 15:53:00
+2, "hazel nut", 10/11/2022 16:01:00
+3, "hazel nut", 10/11/2022 16:01:00
+4, "Bay Guette", 10/11/2022 16:04:50
+5, "Francis Toast", 10/11/2022 16:12:22
+6, "Joe Nut", 10/11/2022 16:44:20 
+7, "Joe Nut", 10/11/2022 16:44:20
+8, "Siri Elle", 10/11/2022 17:22:00
+9, "Muf Finn", 10/11/2022 17:25:33
+10, "Bana Anna", 10/11/2022 17:56:48
+11, "Ote Mell", 10/11/2022 18:55:38
+12, "Cinna Mon", 10/11/2022 22:22:33
