f6230e3079b95f53d33c30a5daac0d01144ebffa
DMorg5598 <113949079+DMorg5598@users.noreply.github.com>
2022-10-14 19:58:21 -0500

    Update pythonfilereader.py

--- pythonfilereader.py
+++ pythonfilereader.py
@@ -1,49 +1,46 @@
 ## ATTEMPT 1
-visits = []
-visit1 = 0
-visit2 = 0
-strikes = 0
-totalvisits = 0
-name1 = 0
-name2 = 0
-time1 = 0
-time2 = 0
-x = 1
-## ReadCycle Function
-f = open("usage.txt", "r")
-while x in f:
-    visits.append(f.readline())
-    totalvisits += 1
-    continue
-## NameMatcher Function
-def namechecker(name1,name2)
-    if name1 == name2:
-        continue
-myfunction()
-## TimeMatcher Function
-        time1 = visit1[18:35]
-        time2 = visit2[18:35]
-        if time1 == time2:
-            stikes += 1
-    else:
-        continue
-print(strikes)
-print("Total visit = ")
-print(totalvisits)
-
-## ATTEMPT 2
+## take data and store it in a dataframe as "usagedataframe"
 import pandas as pd
 namelist = []
-vdf = []
-doublevisits = 0
+visitlist = []
+visitidlist = []
+doubleswipelist = []
+doubleswipes = 0
 udf = pd.read_csv('usage.csv')
-namelist.extend(udf.get("membername"))
-for i in range(len(namelist)):
-  j = i + 1
-  name1 = namelist[i]
-  name2 = namelist[j]
-  if name1 == name2:
-   print("These names match")
-   doublevisits += 1
-print("Double Visits:")
-print(doublevisits)
+newudf = udf.sort_values(by = 'membername')
+## sort the list
+## take the visitid column and store each # in a list a called "idlist"
+visitidlist.extend(newudf.get("visitid"))
+## take the column membername and store each name in a list called "namelist"
+namelist.extend(newudf.get("membername"))
+## take the visit column and store each time in a list called "visitlist"
+visitlist.extend(newudf.get("visittime"))
+## the name function determines if the names match
+def namefunction(name1):
+    if name1 == name2:
+        lastperson = name2
+        return
+        True
+## the time function determines if the visits are within three hours of each other
+def timefunction(visit1, visit2):
+    if visit1 - visit2 < 180  or visit2 - visit1 < 180:
+        return
+        True
+## loop through the list and pass the names through the namefunction
+i = 0
+while i < len(namelist):
+    i = i + 1
+    name1 = namelist[i]
+    namefunction(name1,name2)
+    if namefunction(name1,name2) == True:
+        visit1 = visitlist[i]
+        visit2 = visitlist[j]
+        if visitfunction(visit1,visit2) == True:
+            doubleswipelist.extend(x)
+            doubleswipes += 1
+    if i == 12:
+        break
+print("these visit id numbers")
+print(doubleswipelist)
+print("are double swipes")
+print(doubleswipes)
