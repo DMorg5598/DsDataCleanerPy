3343c6898165e4237afb4c71052717613c641dbc
DMorg5598 <113949079+DMorg5598@users.noreply.github.com>
2022-10-16 23:07:12 -0500

    Update pythonfilereader.py

--- pythonfilereader.py
+++ pythonfilereader.py
@@ -1,46 +1,48 @@
 ## ATTEMPT 1
+## THIS FUNCTION WILL READ FOB SWIPES AND DETERMINE IF THEY ARE WITHIN THREE HOURS OF EACH OTHER
+## the name function determines if the names match
+def namefunction(member1,member2):
+    if member1 in member2:
+        return True
+## the time function determines if the visits are within three hours of each other
+def timefunction(visit1, visit2):
+    if visit1 - visit2 < 180  or visit2 - visit1 < 180:
+        return True
 ## take data and store it in a dataframe as "usagedataframe"
 import pandas as pd
+## create the name, visitid, and visitdate lists and variables
 namelist = []
 visitlist = []
 visitidlist = []
 doubleswipelist = []
 doubleswipes = 0
+dsmessage = "Total Doubleswipes:"
 udf = pd.read_csv('usage.csv')
-newudf = udf.sort_values(by = 'membername')
-## sort the list
+## group the list by name
+newudf = udf.sort_values("membername")
 ## take the visitid column and store each # in a list a called "idlist"
 visitidlist.extend(newudf.get("visitid"))
 ## take the column membername and store each name in a list called "namelist"
 namelist.extend(newudf.get("membername"))
+lastperson = namelist[0]
 ## take the visit column and store each time in a list called "visitlist"
 visitlist.extend(newudf.get("visittime"))
-## the name function determines if the names match
-def namefunction(name1):
-    if name1 == name2:
-        lastperson = name2
-        return
-        True
-## the time function determines if the visits are within three hours of each other
-def timefunction(visit1, visit2):
-    if visit1 - visit2 < 180  or visit2 - visit1 < 180:
-        return
-        True
-## loop through the list and pass the names through the namefunction
+lastvisit = visitlist[0]
+## set the loop stop to the list length, then loop through the list
+totalvisits = max(visitidlist)
 i = 0
-while i < len(namelist):
-    i = i + 1
+while i < len(visitidlist):
     name1 = namelist[i]
-    namefunction(name1,name2)
+    name2 = lastperson
+    time1 = visitlist[i]
+    time2 = lastvisit
+    i += 1
+## if the names match, continue to the time function
     if namefunction(name1,name2) == True:
-        visit1 = visitlist[i]
-        visit2 = visitlist[j]
-        if visitfunction(visit1,visit2) == True:
-            doubleswipelist.extend(x)
+## if the times also match, count it as a double swipe
+        if timefunction(time1,time2) == True:
+            print("this is a doubleswipe")
             doubleswipes += 1
-    if i == 12:
-        break
-print("these visit id numbers")
-print(doubleswipelist)
-print("are double swipes")
-print(doubleswipes)
+            lastvisit = time1
+    lastperson = name1
+print(dsmessage,doubleswipes)
