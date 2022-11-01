277f13201e1beba32893826f2488ecd0389b3e09
DMorg5598 <113949079+DMorg5598@users.noreply.github.com>
2022-11-01 17:54:18 -0500

    Update downvalidator.py

--- downvalidator.py
+++ downvalidator.py
@@ -8,7 +8,8 @@ agmflist = []
 accdtlist = []
 acctslist = []
 pifdtlist = []
-pifdf = pd.read_csv("cashpif.csv")
+pifamtlist = []
+pifdf = pd.read_csv("transactions.csv")
 mdf = pd.read_csv("members.csv")
 cdf = pd.read_csv("cancels.csv")
 agmlist.extend(mdf.get("agreementnumber"))
@@ -17,6 +18,7 @@ amtlist.extend(mdf.get("downpayment"))
 datelist.extend(mdf.get("date"))
 cnclist.extend(cdf.get("membername"))
 pifdtlist.extend(pifdf.get("date"))
+pifamtlist.extend(pifdf.get("amount"))
 def activevalid(name1):
     if name not in cnclist:
         return True
@@ -24,7 +26,7 @@ def amtvalid(amount1):
     if amount1 < 100 and amount1 > 25:
         return True
 def pifvalid(date1,pamt1):
-    if date1 in pifdtlist:
+    if date1 not in pifdtlist and pamt1 > 380:
         return True
 i = 0
 while i < len(nmelist):
@@ -37,12 +39,14 @@ while i < len(nmelist):
             agmflist.append(agmt)
             acctslist.append(name)
             accdtlist.append(date)
-        elif pifvalid(date) == True:
+        elif pifvalid(date,amt) == True:
             agmflist.append(agmt)
             acctslist.append(name)
             accdtlist.append(date)
     i += 1
     if i == len(nmelist):
         break
-for j in agmflist:
-    print(acctslist+accdtlist)
+j = 0
+while j < len(agmflist):
+    print(agmflist[j],acctslist[j],datelist[j])
+    j += 1
